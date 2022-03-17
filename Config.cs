553a7e81c688d7a3d32ffaed38ec2ccbf2a1904c
Pobx <geidtiphong.s@allrightbox.com>
2022-03-17 14:23:04 +0700

    initialize project

---
+++ Config.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace identity4Demo {
+  public static class Config {
+    public static IEnumerable<IdentityResource> IdentityResources =>
+      new IdentityResource[] {
+        new IdentityResources.OpenId ()
+      };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+      new ApiScope[] {
+        new ApiScope ("api1", "API 1")
+      };
+
+    public static IEnumerable<Client> Clients =>
+      new Client[] {
+        new Client {
+        ClientId = "pobx",
+        AllowedGrantTypes = GrantTypes.ClientCredentials,
+        ClientSecrets = {
+        new Secret ("secret1234".Sha256 ())
+        },
+        AllowedScopes = { "api1" }
+        }
+      };
+  }
+}
